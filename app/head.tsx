798df79e90b73b298ac226aef7cf718f82dbae6a
kaZantip_xD <maks22552@gmail.com>
2023-06-18 22:53:40 +0200

    repo init

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+	return (
+		<>
+			<title>30 Days React tutorial</title>
+			<meta content='width=device-width, initial-scale=1' />
+			<link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
+		</>
+	);
+}
---
+++ components/PostMetadata.ts
@@ -0,0 +1,6 @@
+export interface PostMetadata {
+  title: string;
+  description: string;
+  // date: string;
+  slug: string;
+}
